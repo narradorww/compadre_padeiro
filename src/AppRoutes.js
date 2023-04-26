35f43c05ab05c0f556819cd2a5e31c39855a59c9
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2023-04-26 10:20:12 -0300

    Create the AppRoute and the pages of the project

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import UserProfile from './pages/UserProfile';
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-        <Switch>
-            <Route path="/" component={Home} />
-        </Switch>
-        </BrowserRouter>
-    )
-    }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/user-profile" component={UserProfile} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;

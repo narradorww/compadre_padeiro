330413671da9463988077fe03158745d7b35ad14
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2023-04-27 16:31:32 -0300

    created the authenticator widget

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,7 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
 import { ThemeContext } from "../../ThemeContext";
 import { useTranslation } from "react-i18next";
-import logoDark from "../../assets/img/CP-preto.png";
-import logoLight from "../../assets/img/CP-branco.png";
+import Authenticator from "../../components/Authenticator";
 import "./style.scss";
 
 const HomePage = () => {
@@ -13,19 +11,8 @@ const HomePage = () => {
   return (
     <div>
       <div className={`home-page${darkTheme ? " dark-theme" : " light-theme"}`}>
-        <img
-          src={darkTheme ? logoLight : logoDark}
-          alt="Compadre Padeiro"
-          className="logo"
-        />
-        <div className="title">{t('homePage.title')}</div>
-        <div className="auth-options">
-          <Link to="/login" className="auth-option">
-          <div>{t('homePage.login')}</div>
-          </Link>
-          <Link to="/register" className="auth-option">
-          <div>{t('homePage.register')}</div>
-          </Link>
+        <div className="authenticator-container">
+    <Authenticator />
         </div>
       </div>
     </div>
